41a2104e49c49432c00ef8af21e1cbad163afe6c
Daan Depaepe <daan@around.media>
2020-02-28 10:52:15 +0100

    open policy link in new tab

--- src/components/CookieBannerContent.js
+++ src/components/CookieBannerContent.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import CookieOption from './CookieOption';
-import bannerStyle from './bannerStyle';
+import React from "react";
+import CookieOption from "./CookieOption";
+import bannerStyle from "./bannerStyle";
 
 export default (props = {}) => {
   const {
     styles = {},
-    className = '',
-    message = 'No text',
-    policyLink = '/#',
-    privacyPolicyLinkText = 'Privacy Policy',
-    necessaryOptionText = 'Necessary',
-    preferencesOptionText = 'Preferences',
-    statisticsOptionText = 'Statistics',
-    marketingOptionText = 'Marketing',
+    className = "",
+    message = "No text",
+    policyLink = "/#",
+    privacyPolicyLinkText = "Privacy Policy",
+    necessaryOptionText = "Necessary",
+    preferencesOptionText = "Preferences",
+    statisticsOptionText = "Statistics",
+    marketingOptionText = "Marketing",
     showDeclineButton = false,
-    acceptButtonText = 'Accept',
-    declineButtonText = 'Decline',
+    acceptButtonText = "Accept",
+    declineButtonText = "Decline",
     showPreferencesOption = true,
     showStatisticsOption = true,
     showMarketingOption = true,
@@ -23,7 +23,7 @@ export default (props = {}) => {
     onToggleStatisticsCookies = Function,
     onToggleMarketingCookies = Function,
     onDecline = Function,
-    onConfirm = Function,
+    onConfirm = Function
   } = props;
 
   const {
@@ -36,15 +36,21 @@ export default (props = {}) => {
     optionLabel: optionLabelStyle,
     checkbox: checkboxStyle,
     buttonWrapper: buttonWrapperStyle,
-    button: buttonStyle,
+    button: buttonStyle
   } = { ...bannerStyle, ...styles };
 
-  const cookieOptionStyle = { optionWrapperStyle, optionLabelStyle, checkboxStyle };
+  const cookieOptionStyle = {
+    optionWrapperStyle,
+    optionLabelStyle,
+    checkboxStyle
+  };
 
   return (
     <div className={`react-cookie-law-dialog ${className}`} style={dialogStyle}>
       <div className="react-cookie-law-container" style={containerStyle}>
-        <div className="react-cookie-law-msg" style={messageStyle}>{message}</div>
+        <div className="react-cookie-law-msg" style={messageStyle}>
+          {message}
+        </div>
 
         <div className="react-cookie-law-select-pane" style={selectPaneStyle}>
           <CookieOption
@@ -55,54 +61,66 @@ export default (props = {}) => {
             checked
           />
 
-          {
-            showPreferencesOption && (
-              <CookieOption
-                id="check-preferences-cookies"
-                text={preferencesOptionText}
-                styles={cookieOptionStyle}
-                onChange={onTogglePreferencesCookies}
-                checked
-              />
-            )
-          }
+          {showPreferencesOption && (
+            <CookieOption
+              id="check-preferences-cookies"
+              text={preferencesOptionText}
+              styles={cookieOptionStyle}
+              onChange={onTogglePreferencesCookies}
+              checked
+            />
+          )}
 
-          {
-            showStatisticsOption && (
-              <CookieOption
-                id="check-statistics-cookies"
-                text={statisticsOptionText}
-                styles={cookieOptionStyle}
-                onChange={onToggleStatisticsCookies}
-                checked
-              />
-            )
-          }
+          {showStatisticsOption && (
+            <CookieOption
+              id="check-statistics-cookies"
+              text={statisticsOptionText}
+              styles={cookieOptionStyle}
+              onChange={onToggleStatisticsCookies}
+              checked
+            />
+          )}
 
-          {
-            showMarketingOption && (
-              <CookieOption
-                id="check-marketing-cookies"
-                text={marketingOptionText}
-                styles={cookieOptionStyle}
-                onChange={onToggleMarketingCookies}
-              />
-            )
-          }
+          {showMarketingOption && (
+            <CookieOption
+              id="check-marketing-cookies"
+              text={marketingOptionText}
+              styles={cookieOptionStyle}
+              onChange={onToggleMarketingCookies}
+            />
+          )}
         </div>
 
-        <a href={policyLink} className="react-cookie-law-policy" style={policyStyle}>{privacyPolicyLinkText}</a>
+        <a
+          href={policyLink}
+          target="_blank"
+          className="react-cookie-law-policy"
+          style={policyStyle}
+        >
+          {privacyPolicyLinkText}
+        </a>
 
-        <div className="react-cookie-law-button-wrapper" style={buttonWrapperStyle}>
-          {
-            showDeclineButton && (
-              <button type="button" className="react-cookie-law-decline-btn" style={buttonStyle} onClick={() => onDecline()}>
-                <span>{declineButtonText}</span>
-              </button>
-            )
-          }
+        <div
+          className="react-cookie-law-button-wrapper"
+          style={buttonWrapperStyle}
+        >
+          {showDeclineButton && (
+            <button
+              type="button"
+              className="react-cookie-law-decline-btn"
+              style={buttonStyle}
+              onClick={() => onDecline()}
+            >
+              <span>{declineButtonText}</span>
+            </button>
+          )}
 
-          <button type="button" className="react-cookie-law-accept-btn" style={buttonStyle} onClick={() => onConfirm()}>
+          <button
+            type="button"
+            className="react-cookie-law-accept-btn"
+            style={buttonStyle}
+            onClick={() => onConfirm()}
+          >
             <span>{acceptButtonText}</span>
           </button>
         </div>
